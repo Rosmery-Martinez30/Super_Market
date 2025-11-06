3d3cc15d5b6779c069388135cecd0c4a25e5bc1d
Rosmery-Martinez30 <im1005646@gmail.com>
2025-11-06 11:34:54 -0600

    add(cliente, direccion, proveedor,usuario)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { DireccionModule } from './direccion/direccion.module';
+import { ProveedorModule } from './proveedor/proveedor.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, ClienteModule, DireccionModule, ProveedorModule],
   controllers: [AppController],
   providers: [AppService],
 })
